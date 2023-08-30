6fa678f6ec537547cb28e52b229c37e264c19a51
rejivelshan <128121228+rejivelshan@users.noreply.github.com>
2023-08-30 12:08:05 +0530

    Delete MedicineShop directory

--- MedicineShop/src/main/java/com/example/medicineshop/repository/MedicineRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.medicineshop.repository;
-
-import com.example.medicineshop.model.Medicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicineRepository extends JpaRepository<Medicine, Long> {
-   
-}
